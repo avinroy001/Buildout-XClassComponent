2bf34302f72b46580d4dbe08bedbfae08cd74af0
Avinash <avin.roy001@gmail.com>
2024-03-01 16:17:05 +0530

    done commit

--- xclasscomponent/src/App.jsx
+++ xclasscomponent/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import React, { Component } from 'react'
 
 import './App.css'
 
-function App() {
-  
-
-  return (
-    <>
-      
-    </>
-  )
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      count: 0,
+    }
+    this.incre=this.incre.bind(this);
+    this.decre=this.decre.bind(this);
+  }
+  incre(){
+    this.setState(prev=>({
+      count: prev.count+1
+    }));
+  }
+  decre(){
+    this.setState(prev=>({
+      count: prev.count-1,
+    }));
+  }
+  render(){
+    return(
+      <>
+      <h1>Counter App</h1>
+      <p>Count: {this.state.count}</p>
+      <button onClick={this.incre}>Increment</button>
+      <button onClick={this.decre}>Decrement</button>
+      </>
+    )
+  }
 }
 
 export default App
